a79bc202a151f0a65d7d6384217007433d69744b
mohammad.saiyed <Mohammad.Saiyed@eclinicalworks.com>
2020-10-01 05:13:29 -0400

    Initial commit

---
+++ src/main/java/com/techtalk/demo/DemoSpringcloudConfigserverGitApplication.java
@@ -0,0 +1,13 @@
+package com.techtalk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringcloudConfigserverGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringcloudConfigserverGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techtalk/demo/DemoSpringcloudConfigserverGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtalk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringcloudConfigserverGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
