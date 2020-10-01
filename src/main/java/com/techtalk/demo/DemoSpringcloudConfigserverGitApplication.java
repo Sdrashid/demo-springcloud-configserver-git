ba6f5a22dbda88496c2ac5ccd37a55ae7b21929a
mohammad.saiyed <Mohammad.Saiyed@eclinicalworks.com>
2020-10-01 06:59:58 -0400

    changes

--- src/main/java/com/techtalk/demo/DemoSpringcloudConfigserverGitApplication.java
+++ src/main/java/com/techtalk/demo/DemoSpringcloudConfigserverGitApplication.java
@@ -2,8 +2,10 @@ package com.techtalk.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class DemoSpringcloudConfigserverGitApplication {
 
 	public static void main(String[] args) {
